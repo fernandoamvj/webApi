789f358dd403016d3282a4146e1441fdb04fd70f
fernando.martins <fernando.martins@meta3.com.br>
2018-10-09 17:53:42 -0300

    update and delete

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -29,6 +29,38 @@ namespace webapi.Controllers
                 return NotFound();
             return new ObjectResult(usuario);
         } 
+        [HttpPost]
+        public IActionResult Create ([FromBody] Usuario user)
+        {
+            if (user == null)
+                return BadRequest();
+            _usuarioRepositorio.Add(user);
+            return CreatedAtRoute("GetUsuario", new { id = user.UsuarioId }, user);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] Usuario user)
+        {
+            if (user == null || user.UsuarioId != id)
+                return BadRequest();
+            var _usuario = _usuarioRepositorio.Find(id);
+            if (_usuario == null)
+                return NotFound();
+            _usuario.Email = user.Email;
+            _usuario.Nome = user.Nome;
+            _usuarioRepositorio.Update(_usuario);
+            return new NoContentResult();
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var usuario = _usuarioRepositorio.Find(id);
+            if (usuario == null)
+                return NotFound();
+            _usuarioRepositorio.Remove(id);
+            return new NoContentResult();
+        }
 
     }
 }
